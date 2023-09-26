8e98413766e6277c3a5f78322b82f00d6e9d2d44
Pedro Enrique Pereira Velosa <m_pedro.velosa@fiap.com.br>
2023-09-26 17:29:22 -0300

    teste com imagem 4

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -9,7 +9,13 @@ type ProjectProps = {
   image: string;
 };
 
-const Project: FC<ProjectProps> = ({ title, description, demo, source }) => {
+const Project: FC<ProjectProps> = ({
+  title,
+  description,
+  demo,
+  source,
+  image,
+}) => {
   const projectRef = useRef<ElementRef<'article'>>(null);
 
   const mainControls = useAnimation();
@@ -25,7 +31,7 @@ const Project: FC<ProjectProps> = ({ title, description, demo, source }) => {
 
   return (
     <>
-      <div className=" w-full shrink-0 rounded-md bg-slate-50 shadow-md">
+      <div className=" w-full shrink-0 rounded-md bg-slate-100 shadow-md">
         <motion.article
           ref={projectRef}
           className="mx-auto h-full max-w-7xl"
@@ -33,15 +39,17 @@ const Project: FC<ProjectProps> = ({ title, description, demo, source }) => {
           <motion.div className="flex h-full flex-col p-12">
             <h1 className="mb-8 text-3xl font-bold lg:text-4xl">{title}</h1>
             <div className="grid h-full gap-4 lg:grid-cols-[3fr_2fr]">
-              {/* <img
-                src={image}
-                className="mx-auto block aspect-video max-w-md items-stretch rounded-md object-cover lg:order-2 lg:aspect-auto"
-              /> */}
+              <div className="relative mx-auto block aspect-video h-full w-full max-w-xs items-stretch overflow-hidden rounded-md lg:order-2 lg:max-w-md">
+                <img
+                  src={image}
+                  className="absolute inset-0 h-full object-cover object-left"
+                />
+              </div>
               <div className="flex flex-col gap-8">
                 <p className="text-xl">{description}</p>
                 <div className="font-semibold capitalize">
                   <a
-                    className="mr-8 inline-block rounded-md bg-slate-100 px-6 py-2 shadow-lg hover:bg-slate-200"
+                    className="mr-8 inline-block rounded-md bg-slate-200 px-6 py-2 shadow-lg hover:bg-slate-300"
                     target="_blank"
                     href={demo}
                   >
